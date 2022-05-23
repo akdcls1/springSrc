29fc59946565acecaf30e7d37b94fa5061d14f3f
akdcls1 <akdcls1@naver.com>
2022-05-23 13:54:49 +0900

    20220523 오전수업
    Mybatis 추가 수정본

--- oBootMybatis01/src/main/java/com/oracle/oBootMybatis01/contoller/EmpController.java
+++ oBootMybatis01/src/main/java/com/oracle/oBootMybatis01/contoller/EmpController.java
@@ -1,12 +1,17 @@
 package com.oracle.oBootMybatis01.contoller;
 
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.oracle.oBootMybatis01.dao.EmpDao;
 import com.oracle.oBootMybatis01.model.Emp;
 import com.oracle.oBootMybatis01.service.EmpService;
 import com.oracle.oBootMybatis01.service.Paging;
@@ -23,13 +28,52 @@ public class EmpController {
 		logger.info("EmpController Start list...");
 		
 		int total = es.total();	// Emp Count -> 42
-		
-		Paging pg = new Paging(total, currentPage);
+
 		System.out.println("EmpController total->"+total);
-		
+		Paging pg = new Paging(total, currentPage);
+		emp.setStart(pg.getStart());	// 시작시 1
+		emp.setEnd(pg.getEnd());		// 시작시 10
+		List<Emp> listEmp = es.listEmp(emp);
+		System.out.println("EmpController list listEmp.size()=>"+listEmp.size());
+		model.addAttribute("listEmp", listEmp);
+		model.addAttribute("pg", pg);
 		model.addAttribute("total", total);
 		
 		return "list";
+	}
+	
+	@GetMapping(value="detail")
+	public String detail(int empno, Model model) {
+//		Emp emp = es.detail(emp);
+		Emp emp = es.detail(empno);
+//		Dao -> ed.detail(emp);
+//		EmpDao ed = ed.detail(emp);
+//		mapper --> tkEmpSelOne, empno
+		System.out.println("EmpController Start detail...");
+		
+		model.addAttribute("emp", emp);
+		return "detail";
+	}
+	
+	@GetMapping(value="updateForm")
+	public String updateForm(int empno, Model model) {
+		System.out.println("EmpController Start updateForm...");
+		Emp emp = es.detail(empno);
+		model.addAttribute("emp", emp);
+		return "updateForm";
+	}
+	
+	@PostMapping(value="update")
+	public String update(Emp emp, Model model) {
+		int uptCnt = es.update(emp);
+//		int kkk = es.update(emp)
+//		Dao -> ed.update(emp)
+//		mapper -> tkEmpUpdate , emp
+		
+		System.out.println("es.update(emp) Count --> "+uptCnt);
+		model.addAttribute("uptCnt", uptCnt);
+		model.addAttribute("kk3", "Message Test");
+		return "redirect:list";
 	}
 
 }
--- oBootMybatis01/src/main/java/com/oracle/oBootMybatis01/dao/EmpDaoImpl.java
+++ oBootMybatis01/src/main/java/com/oracle/oBootMybatis01/dao/EmpDaoImpl.java
@@ -1,8 +1,12 @@
 package com.oracle.oBootMybatis01.dao;
 
+import java.util.List;
+
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+
+import com.oracle.oBootMybatis01.model.Emp;
 
 @Repository
 public class EmpDaoImpl implements EmpDao {
@@ -17,13 +21,45 @@ public class EmpDaoImpl implements EmpDao {
 		
 		try {
 			// Naming Rule(조별로 정하래) ---> Map ID
-			tot = session.selectOne("tkEmpTotal");
+			tot = session.selectOne("tkEmpTotal");	// " "안에 있는 값을 통해서 Mybatis를 찾아간다.
 			System.out.println("EmpDaoImpl total tot->"+tot);
 		} catch (Exception e) {
 			System.out.println("EmpDaoImpl total Exception->"+e.getMessage());
 		}
 		
 		return tot;
+	}
+
+	@Override
+	public List<Emp> listEmp(Emp emp) {
+		List<Emp> empList = null;
+		System.out.println("EmpDaoImpl listEmp Start...");
+		try {
+			//	Naming Rule					Map ID		parameter
+			empList = session.selectList("tkEmpListAll3", emp);
+			System.out.println("EmpDaoImpl listEmp empList.size()->"+empList.size());
+			for(Emp rtnEmp : empList) {
+				System.out.println("EmpDaoImpl listEmp rtnEmp.getEname()->"+rtnEmp.getEname());
+				System.out.println("EmpDaoImpl listEmp rtnEmp.getSal()->"+rtnEmp.getSal());
+			}
+		} catch (Exception e) {
+			System.out.println("EmpDaoImpl listEmp Exception->"+e.getMessage());
+		}
+		return empList;
+	}
+
+	@Override
+	public Emp detail(int empno) {
+		Emp emp = new Emp();
+		System.out.println("EmpDaoImpl detail Start...");
+		try {
+			//						mapper ID	, Parameter
+			emp = session.selectOne("tkEmpSelOne", empno);
+			System.out.println("EmpDaoImpl detail getEname->"+emp.getEname());
+		} catch (Exception e) {
+			System.out.println("EmpDaoImpl detail Exception->"+e.getMessage());
+		}
+		return emp;
 	}
 
 }
